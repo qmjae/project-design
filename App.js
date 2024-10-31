8232c216d92e25ea036194f60d3db05376694cd7
qmjae <142195148+qmjae@users.noreply.github.com>
2024-11-01 00:54:26 +0800

    feat: added results page

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { createStackNavigator } from '@react-navigation/stack';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import HomeScreen from './screens/HomeScreen';
 import AnalysisScreen from './screens/AnalysisScreen';
+import ResultsScreen from './screens/ResultsScreen';
 
 const Stack = createStackNavigator();
 
@@ -14,6 +15,7 @@ export default function App() {
         <Stack.Navigator screenOptions={{ headerShown: false }}>
           <Stack.Screen name="Home" component={HomeScreen} />
           <Stack.Screen name="Analysis" component={AnalysisScreen} />
+          <Stack.Screen name="Results" component={ResultsScreen} />
         </Stack.Navigator>
       </NavigationContainer>
     </SafeAreaProvider>
