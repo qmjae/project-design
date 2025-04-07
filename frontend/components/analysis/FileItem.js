9a52374d1fe21167f0ca90f386265748b866794a
qmjae <qmjaejercito@tip.edu.ph>
2025-04-07 10:12:18 +0800

    for prod fix

--- frontend/components/analysis/FileItem.js
+++ frontend/components/analysis/FileItem.js
@@ -4,10 +4,17 @@ import Animated, {
   FadeOut,
   FadeIn,
   LinearTransition,
-  SlideOutRight
+  SlideOutRight,
+  configureReanimatedLogger,
+  ReanimatedLogLevel,
 } from 'react-native-reanimated';
 import Ionicons from '@expo/vector-icons/Ionicons';
 
+// This is the default configuration
+configureReanimatedLogger({
+  level: ReanimatedLogLevel.warn,
+  strict: false, // Reanimated runs in strict mode by default
+});
 export const FileItem = memo(({ file, onRemove }) => (
   <Animated.View 
     entering={FadeIn}
