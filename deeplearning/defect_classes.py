3fea9263bfb9c4c8831873de291d7d4f0d569677
qmjae <qmjaejercito@tip.edu.ph>
2025-04-14 23:40:50 +0800

    fix based on objective #2

--- deeplearning/defect_classes.py
+++ deeplearning/defect_classes.py
@@ -12,24 +12,11 @@ class DefectInfo:
 # Define all defect classes
 DEFECT_CLASSES = {
     #DONE
-    "partial-shading": DefectInfo(
-        className="Partial Shading",
-        stressFactors=["N/A"],
-        severityLevel="5 - Moderate",
-        powerLoss="~10%-20% reduction",
-        category="IEC TS 62446-3: Local abnormalities",
-        CoA="2: thermal abnormality – tA",
-        description="Assessable by thermal pattern, visual image and classified typically as an extended area abnormality. ",
-        recommendations=[
-            "Cleaning of PV modules is highly recommended in near future to avoid damage of PV module, proper care can significantly minimize the probability of partial shading and new shading elements",
-        ]
-    ),
-    #DONE
     "short-circuit": DefectInfo(
         className="Short Circuit",
         stressFactors=["Thermal Cycling", "Mechanical Load"],
         severityLevel="9 - Hazardous",
-        powerLoss="No power output, unsafe for maintenance",
+        powerLoss="No power output",
         category="IEC TS 62446-3: Strings and modules",
         CoA="2: thermal abnormality – tA",
         description="Assessable by thermal pattern, visual image and classified as a extended area abnormality. Similar pattern as with broken front glass (check isolation resistance), PID, cell defects and mismatch. ",
@@ -55,7 +42,7 @@ DEFECT_CLASSES = {
         className="Bypass Diode Failure",
         stressFactors=["Electrical"], #DONE
         severityLevel="7 - High", #DONE
-        powerLoss=">50% reduction", #DONE
+        powerLoss="Leakage current drop", #DONE 
         category="IEC TS 62446-3:  Substrings within module", #DONE
         CoA="2: thermal abnormality – tA", #DONE
         description="Assessable by thermal pattern and classified as  a extended area abnormality.  At one or more substrings, easily mistaken for cell breakage or cell defects, Potential induced degradation (PID) or mismatch.", #DONE
@@ -68,7 +55,7 @@ DEFECT_CLASSES = {
         className="Single Cell",
         stressFactors=["Temperature", "UV"], #DONE
         severityLevel="7 - High", #DONE
-        powerLoss=">50% reduction", #DONE
+        powerLoss="High", #DONE
         category="IEC TS 62446-3:  Local abnormalities", #DONE
         CoA="2: thermal abnormality or tA or 3: safety relevant thermal abnormality – dtA", #DONE
         description="Assessable by thermal pattern, visual image and classified as an extended area abnormality. Difference in temperature increases with load, cell efficiency and number of cells in a substring. High temperatures mostly caused by broken cells. Might lead to irreversible damage of cell, encapsulation and bypass diodes.",
