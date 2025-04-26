1a60154329419ef318566059889cf456fda14ff2
qmjae <qmjaejercito@tip.edu.ph>
2025-04-26 09:48:11 +0800

    removed unused files, bumped dependencies

--- frontend/config.js
+++ frontend/config.js
@@ -1,5 +1,10 @@
 // Configuration file for backend API endpoints and other constants
 
-export const BACKEND_API_URL = "http://192.168.191.150:8000/detect/";
-export const CAMERA_URL = "http://192.168.191.150:5000/camera";
-export const SNAPSHOT_API_URL = "http://192.168.191.150:5000/snapshot";
+// export const BACKEND_API_URL = "http://192.168.191.150:8000/detect/";
+// export const CAMERA_URL = "http://192.168.191.150:5000/camera";
+// export const SNAPSHOT_API_URL = "http://192.168.191.150:5000/snapshot";
+
+
+export const BACKEND_API_URL = "http://192.168.1.18:8000/detect/";
+export const CAMERA_URL = "http://192.168.1.18:5000/camera";
+export const SNAPSHOT_API_URL = "http://192.168.1.18:5000/snapshot";
